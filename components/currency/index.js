021feb8c9e074d8d5142f971bc672ad0bd487d9d
Kusal KC <kckusal99@gmail.com>
2020-11-28 02:32:03 +0300

    add utility components

---
+++ components/currency/index.js
@@ -0,0 +1,38 @@
+import { useSelector } from "react-redux";
+
+import { Flex, Box } from "@chakra-ui/react";
+import CurrencyFormat from "react-currency-format";
+
+function Currency({
+  value,
+  currency = "usd",
+  noPrefix = false,
+  noSuffix = false,
+  ...restProps
+}) {
+  const { prefix, suffix } = useSelector(state => state.app.currency);
+
+  return (
+    <Flex align="center" {...restProps}>
+      {!noPrefix && (
+        <Box as="span" mr={1} fontWeight="500">
+          {prefix}
+        </Box>
+      )}
+
+      <CurrencyFormat
+        value={value}
+        displayType="text"
+        thousandSeparator={true}
+      />
+
+      {!noSuffix && (
+        <Box as="span" ml={1}>
+          {suffix}
+        </Box>
+      )}
+    </Flex>
+  );
+}
+
+export default Currency;

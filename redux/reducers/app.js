514a35a66167ad79189708f485683afb0941dc91
Kusal KC <kckusal99@gmail.com>
2020-11-29 23:46:40 +0300

    update currency state to show when menu is first loaded

--- redux/reducers/app.js
+++ redux/reducers/app.js
@@ -4,16 +4,30 @@ const initialState = {
   darkMode: false,
 
   currency: {
-    currentCode: "usd",
-    allCodes: ["usd", "eur"],
+    currentCode: null,
+    allCodes: ["usd", "eur", "rub"],
     optionsByCode: {
       usd: {
         label: "US Dollar",
         symbol: "$",
         prefix: "$",
-        suffix: ""
+        suffix: "",
+        ratePerDollar: 1
       },
-      eur: { label: "Euro", symbol: "€", prefix: "€", suffix: "" }
+      eur: {
+        label: "Euro",
+        symbol: "€",
+        prefix: "€",
+        suffix: "",
+        ratePerDollar: 0.84
+      },
+      rub: {
+        label: "Ruble",
+        symbol: "₽",
+        prefix: "",
+        suffix: "руб",
+        ratePerDollar: 76.08
+      }
     }
   },
 
@@ -48,6 +62,19 @@ export default function reducer(state = initialState, action) {
           ? state.toasts.filter(toast => toast.id !== toastId)
           : []
       };
+    }
+
+    case actionTypes.LOAD_MENU: {
+      const items = action.payload.items;
+
+      if (!state.currency.currentCode && items.length > 0) {
+        return {
+          ...state,
+          currency: { ...state.currency, currentCode: items[0].currencyCode }
+        };
+      }
+
+      return state;
     }
 
     default:

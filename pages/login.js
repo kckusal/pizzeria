8fa2efb8dd1c01d9a35cec8da6fd5c44df89aa3a
Kusal KC <kckusal99@gmail.com>
2020-11-29 23:45:22 +0300

    validation added for login fields

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,60 @@
+import { useState, useRef } from "react";
+import { useDispatch } from "react-redux";
+
 import {
   Heading,
   Stack,
   FormControl,
   FormLabel,
-  Input,
+  FormErrorMessage,
   Button
 } from "@chakra-ui/react";
 
 import AppContainer from "components/AppContainer";
+import EmailInput from "components/EmailInput";
 import PasswordInput from "components/PasswordInput";
 import Link from "components/Link";
+import { addToast } from "redux/actions";
 
 function Login() {
+  const dispatch = useDispatch();
+
+  const data = useRef({
+    email: "",
+    password: ""
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    password: ""
+  });
+
+  const setError = (key, error) => {
+    setErrors({ ...errors, [key]: error });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (data.current.email && data.current.password) {
+      dispatch(
+        addToast({
+          status: "success",
+          title: "Login successful.",
+          description: "Login will proceed now."
+        })
+      );
+    } else {
+      dispatch(
+        addToast({
+          status: "error",
+          title: "Invalid data",
+          description: "Make sure you have provided valid email and password."
+        })
+      );
+    }
+  };
+
   return (
     <AppContainer height="full" py={8}>
       <Stack
@@ -27,20 +70,51 @@ function Login() {
           Login
         </Heading>
 
-        <Stack pt={6} spacing={6} justify="center">
-          <FormControl isRequired>
+        <Stack
+          as="form"
+          noValidate
+          pt={6}
+          spacing={6}
+          justify="center"
+          onSubmit={handleSubmit}
+        >
+          <FormControl isRequired isInvalid={errors.email}>
             <FormLabel htmlFor="email">Email</FormLabel>
-            <Input type="email" id="email" placeholder="j.doe@example.com" />
+            <EmailInput
+              type="email"
+              id="email"
+              placeholder="j.doe@example.com"
+              onChange={value => {
+                data.current.email = value;
+                setError("email", "");
+              }}
+              onError={err => {
+                setError("email", err);
+              }}
+            />
+            <FormErrorMessage>{errors.email}</FormErrorMessage>
           </FormControl>
 
-          <FormControl isRequired>
+          <FormControl isRequired isInvalid={errors.password}>
             <FormLabel htmlFor="password">Password</FormLabel>
-            <PasswordInput id="password" onChange={() => {}} />
+            <PasswordInput
+              id="password"
+              onChange={value => {
+                if (value.trim() === "") {
+                  setError("password", "Password is required.");
+                } else {
+                  data.current.password = value;
+                  setError("password", "");
+                }
+              }}
+            />
+            <FormErrorMessage>{errors.password}</FormErrorMessage>
           </FormControl>
 
           <Link href="#" mb={1} colorScheme="blue">
             Forgot Password ?
           </Link>
+
           <Button
             type="submit"
             colorScheme="primary"
@@ -53,7 +127,7 @@ function Login() {
           </Button>
         </Stack>
 
-        <Link href="/register" mt="0">
+        <Link href="/register" mt="0" color="blue.500">
           Do not have an account yet? Click to register.
         </Link>
       </Stack>

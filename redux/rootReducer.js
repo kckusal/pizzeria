785632ac158e6a91a96c269dd82267633c8e368f
Kusal KC <kckusal99@gmail.com>
2020-11-29 11:41:25 +0300

    added menu items in redux store

--- redux/rootReducer.js
+++ redux/rootReducer.js
@@ -5,12 +5,14 @@ import { HYDRATE } from "next-redux-wrapper";
 import { actionTypes } from "./actions";
 import app from "./reducers/app";
 import constants from "./reducers/constants";
+import menu from "./reducers/menu";
 import cart from "./reducers/cart";
 import user from "./reducers/user";
 
 const mainReducer = combineReducers({
   app,
   constants,
+  menu,
   cart,
   user
 });

200946606a712dd9f4bc898a9930799fa1e5a2d6
Kusal KC <kckusal99@gmail.com>
2020-12-03 18:29:03 +0300

    update apis to use Mongodb db

--- pages/api/menu.js
+++ pages/api/menu.js
@@ -1,16 +1,26 @@
-const fs = require("fs");
-const path = require("path");
-
-const menuFileDir = path.join(process.cwd(), "data/menu.json");
+import { connectToDatabase } from "utils/mongodb";
+import { cors, runMiddleware } from "utils/apiMiddlewares";
 
 export function getMenuItems() {
-  const rawData = fs.readFileSync(menuFileDir);
-  return JSON.parse(rawData);
+  return connectToDatabase()
+    .then(response => response.db)
+    .then(db =>
+      db
+        .collection("menu")
+        .find({})
+        .map(item => ({ ...item, _id: item._id.toString() }))
+        .toArray()
+    );
 }
 
-export default (req, res) => {
+export default async (req, res) => {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
   if (req.method === "GET") {
-    return res.status(200).json({ menu: getMenuItems() });
+    return getMenuItems()
+      .then(menu => res.status(200).json({ menu }))
+      .catch(e => res.status(500).json(e));
   }
 
   res.status(204);

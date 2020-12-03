64828bb15a7b35acf08ffa9ae0e3ebf01032a2bd
Kusal KC <kckusal99@gmail.com>
2020-12-03 18:26:27 +0300

    update

--- redux/rootReducer.js
+++ redux/rootReducer.js
@@ -6,21 +6,19 @@ import { actionTypes } from "./actions";
 import app from "./reducers/app";
 import constants from "./reducers/constants";
 import menu from "./reducers/menu";
-import cart from "./reducers/cart";
 import user from "./reducers/user";
 
 const mainReducer = combineReducers({
   app,
   constants,
   menu,
-  cart,
   user
 });
 
 const rootReducer = (state, action) => {
   switch (action.type) {
     case HYDRATE: {
-      return { ...state, ...action.payload };
+      return state;
     }
 
     default:
